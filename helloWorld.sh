d9ac4372fbd315663a40ff5c4f3a5c0b7bedc20d
juniorDeveloper-setName("austBrink") <43417690+austBrink@users.noreply.github.com>
2022-09-30 08:26:07 -0500

    expr first

--- helloWorld.sh
+++ helloWorld.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/bash
-echo "Hello, World!"
+# make a comment like this one. 
+
+# When setting variables, note the spacing... 
+greeting=Hello 
+user=World
+# echo "Hello, World!"
+echo $greeting", "$user 
 sleep 2 
+echo Of course, bash can do math, which is kinda important...
+echo expr 2 + 2 is 
+expr 2 + 2
 echo "Goodbye!"
 
